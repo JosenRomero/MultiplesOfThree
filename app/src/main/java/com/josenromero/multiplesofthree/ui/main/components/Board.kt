b0b3d0df2dae27645768b27fe96221e9d3f3b0f5
Jose <romerojosemanuel@hotmail.com>
2024-10-08 15:40:28 -0600

    change design to board

--- app/src/main/java/com/josenromero/multiplesofthree/ui/main/components/Board.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/main/components/Board.kt
@@ -4,19 +4,21 @@ import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.josenromero.multiplesofthree.ui.theme.MultiplesOfThreeTheme
 import com.josenromero.multiplesofthree.utils.Constants
 
@@ -25,7 +27,11 @@ fun Board(
     board: List<List<Int>>
 ) {
 
-    LazyColumn() {
+    LazyColumn(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.secondary),
+        contentPadding = PaddingValues(5.dp)
+    ) {
         items(items = board) {row ->
 
             LazyRow {
@@ -41,18 +47,24 @@ fun Board(
 @Composable
 fun TableCell(item: Int) {
 
-    val content = if (item == Constants.DEFAULT_VALUE) "" else item.toString()
+    val emptyCell = item == Constants.DEFAULT_VALUE
 
     Box(
         modifier = Modifier
             .width(100.dp)
             .height(100.dp)
-            .background(color = Color.Cyan)
-            .border(3.dp, Color.Gray)
-            .padding(8.dp),
+            .background(
+                color = if (emptyCell) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.onSecondary
+            )
+            .border(5.dp, MaterialTheme.colorScheme.secondary)
+            .padding(16.dp),
         contentAlignment = Alignment.Center
     ) {
-        Text(content)
+        Text(
+            text = if (emptyCell) "" else item.toString(),
+            color = MaterialTheme.colorScheme.onTertiary,
+            fontSize = 16.sp
+        )
     }
 
 }
