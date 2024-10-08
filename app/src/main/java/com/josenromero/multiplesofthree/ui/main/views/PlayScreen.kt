b920badf160dca99c81cd7340765b188161c7146
Jose <romerojosemanuel@hotmail.com>
2024-10-08 15:39:33 -0600

    add background in PlayScreen

--- app/src/main/java/com/josenromero/multiplesofthree/ui/main/views/PlayScreen.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/main/views/PlayScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +22,9 @@ fun PlayScreen(
     board: List<List<Int>>
 ) {
 
-    Scaffold {
+    Scaffold(
+        containerColor = MaterialTheme.colorScheme.background
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
