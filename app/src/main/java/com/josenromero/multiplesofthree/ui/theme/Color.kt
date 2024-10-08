dfa3dacd01af7e52a2b51985f3a153a4fa79e103
Jose <romerojosemanuel@hotmail.com>
2024-10-08 15:37:26 -0600

    change colors

--- app/src/main/java/com/josenromero/multiplesofthree/ui/theme/Color.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.josenromero.multiplesofthree.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primaryLight = Color(0xFF8F7A66)
+val onPrimaryLight = Color(0xFFEEE4DA)
+val secondaryLight = Color(0xFFBBADA0)
+val onSecondaryLight = Color(0xFFEEE4DA)
+val tertiaryLight = Color(0xFFCDC1B4)
+val onTertiaryLight = Color(0xFF776E65)
+val backgroundLight = Color(0xFFFAF8EF)
