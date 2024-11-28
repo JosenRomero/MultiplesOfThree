2dfa76a1a5ad7fcd4c0b503cb137699aabefe648
Jose <romerojosemanuel@hotmail.com>
2024-11-28 15:57:23 -0600

    add CustomTitle component

---
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/main/components/CustomTitle.kt
@@ -0,0 +1,33 @@
+package com.josenromero.multiplesofthree.ui.main.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CustomTitle(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onPrimary,
+    fontSize: TextUnit = 20.sp,
+    fontWeight: FontWeight = FontWeight.SemiBold,
+    textAlign: TextAlign = TextAlign.Center
+) {
+
+    Text(
+        text = text,
+        modifier = modifier.fillMaxWidth(),
+        color = color,
+        fontSize = fontSize,
+        fontWeight = fontWeight,
+        textAlign = textAlign
+    )
+
+}
