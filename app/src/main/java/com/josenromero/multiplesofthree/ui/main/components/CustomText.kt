0f0fb408bcc2876cc69d9603249a8dd947b15786
Jose <romerojosemanuel@hotmail.com>
2024-12-19 18:08:03 -0600

    add CustomText component

---
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/main/components/CustomText.kt
@@ -0,0 +1,32 @@
+package com.josenromero.multiplesofthree.ui.main.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CustomText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onPrimary,
+    fontSize: TextUnit = 14.sp,
+    fontWeight: FontWeight = FontWeight.Normal,
+    textAlign: TextAlign = TextAlign.Start
+) {
+
+    Text(
+        text = text,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontWeight = fontWeight,
+        textAlign = textAlign
+    )
+
+}
