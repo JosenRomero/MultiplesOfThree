6a03a8fac6b0f0801642e8b45512ee142a3fc702
Jose <romerojosemanuel@hotmail.com>
2024-12-25 16:30:11 -0600

    add game mode translations

--- app/src/main/java/com/josenromero/multiplesofthree/data/GameMode.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/data/GameMode.kt
@@ -1,7 +1,10 @@
 package com.josenromero.multiplesofthree.data
 
-enum class GameMode {
-    EASY,
-    NORMAL,
-    HARD
+import androidx.annotation.StringRes
+import com.josenromero.multiplesofthree.R
+
+enum class GameMode(@StringRes val textId: Int) {
+    EASY(textId = R.string.home_screen_menu_easy_mode),
+    NORMAL(textId = R.string.home_screen_menu_normal_mode),
+    HARD(textId = R.string.home_screen_menu_hard_mode)
 }
