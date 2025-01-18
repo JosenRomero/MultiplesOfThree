78a02ee49cd434c40e839e853b8d3feeeb9522e5
Jose <romerojosemanuel@hotmail.com>
2025-01-18 16:59:48 -0600

    change color to CustomBottomAppBar component

--- app/src/main/java/com/josenromero/multiplesofthree/ui/main/components/CustomBottomAppBar.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/main/components/CustomBottomAppBar.kt
@@ -1,15 +1,14 @@
 package com.josenromero.multiplesofthree.ui.main.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -18,17 +17,18 @@ fun CustomBottomAppBar(
 ) {
 
     BottomAppBar(
-        modifier = Modifier
-            .padding(16.dp)
-            .clip(shape = RoundedCornerShape(25.dp)),
-        containerColor = MaterialTheme.colorScheme.secondary
+        containerColor = MaterialTheme.colorScheme.background
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            content()
+        Column {
+            CustomSpacer()
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                content()
+            }
         }
     }
 
