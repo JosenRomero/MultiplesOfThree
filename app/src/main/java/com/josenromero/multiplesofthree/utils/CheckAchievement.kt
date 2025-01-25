3e2e1532820492200dec256cfafa07d7abc39e47
Jose <romerojosemanuel@hotmail.com>
2025-01-25 17:50:15 -0600

    refactor: checkAchievement file

--- app/src/main/java/com/josenromero/multiplesofthree/utils/CheckAchievement.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/utils/CheckAchievement.kt
@@ -5,22 +5,22 @@ import com.josenromero.multiplesofthree.data.GameMode
 
 fun checkAchievement(achievements: List<String>, score: Int, gameMode: GameMode): List<String> {
 
-    val currentAchievements: MutableList<String> = achievements.toMutableList()
+    val newAchievements: MutableList<String> = mutableListOf()
 
     val newAchievement: Achievement? = Constants.achievementsList.find { achievement ->
         achievement.scoreTarget == score && achievement.gameMode == gameMode
     }
 
-    if (newAchievement != null && !currentAchievements.contains(newAchievement.id.toString())) {
-        currentAchievements.add(newAchievement.id.toString()) // save the achievement ID
+    if (newAchievement != null && !achievements.contains(newAchievement.id.toString())) {
+        newAchievements.add(newAchievement.id.toString()) // save the achievement ID
 
         // if complete all game modes then save last medal
-        if (currentAchievements.size == Constants.achievementsList.size - 1) {
+        if ((achievements.size + newAchievements.size) == (Constants.achievementsList.size - 1)) {
             val lastMedal = Constants.achievementsList.last()
-            currentAchievements.add(lastMedal.id.toString())
+            newAchievements.add(lastMedal.id.toString())
         }
     }
 
-    return currentAchievements
+    return newAchievements
 
 }
