22d5d46dce39c03b7a8d81bd4220ddfc49559d45
Jose <romerojosemanuel@hotmail.com>
2025-01-31 16:14:12 -0600

    feat: add confetti animation in endScreen

--- app/src/main/java/com/josenromero/multiplesofthree/ui/main/views/EndScreen.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/main/views/EndScreen.kt
@@ -10,21 +10,44 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.josenromero.multiplesofthree.R
 import com.josenromero.multiplesofthree.ui.main.components.AnimatedTextScale
+import com.josenromero.multiplesofthree.ui.main.components.Confetti
 import com.josenromero.multiplesofthree.ui.main.components.CustomText
 import com.josenromero.multiplesofthree.ui.main.navigation.AppScreens
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun EndScreen(
     textIdForGameMode: Int,
     onNavigateToAScreen: (route: String) -> Unit
 ) {
+
+    var coordinates by remember { mutableStateOf(Offset.Zero) }
+    val isConfetti = remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        launch {
+            delay(1000)
+            isConfetti.value = true
+            delay(1000) // waiting for the confetti animation
+            isConfetti.value = false
+        }
+    }
 
     Scaffold(
         containerColor = MaterialTheme.colorScheme.background
@@ -48,7 +71,13 @@ fun EndScreen(
                     text = stringResource(id = textIdForGameMode) + stringResource(id = R.string.end_screen_congratulations_text_2), // for example "Easy Mode Completed"
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 50.dp, bottom = 20.dp),
+                        .padding(top = 50.dp, bottom = 20.dp)
+                        .onGloballyPositioned { layoutCoordinates ->
+                            val pos = layoutCoordinates.positionInRoot()
+                            val middleX = layoutCoordinates.size.width / 2
+                            val middleY = layoutCoordinates.size.height / 2
+                            coordinates = Offset(x = pos.x + middleX, y = pos.y - middleY)
+                        },
                     textAlign = TextAlign.Center
                 )
                 Button(
@@ -58,6 +87,9 @@ fun EndScreen(
                         text = stringResource(id = R.string.end_screen_congratulations_btn_menu)
                     )
                 }
+            }
+            if (isConfetti.value) {
+                Confetti(initialPosition = coordinates)
             }
         }
     }
