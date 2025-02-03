6011587e7e9ce70b75da6ae77044b23b97ed3f12
Jose <romerojosemanuel@hotmail.com>
2025-02-03 16:18:36 -0600

    add background and padding to the Mission Text

--- app/src/main/java/com/josenromero/multiplesofthree/ui/main/components/MissionText.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/main/components/MissionText.kt
@@ -1,8 +1,13 @@
 package com.josenromero.multiplesofthree.ui.main.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -13,13 +18,23 @@ fun MissionText(
 ) {
 
     AnimatedFadeIn {
-        CustomText(
-            text = text,
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 40.dp),
-            textAlign = TextAlign.Center
-        )
+                .padding(vertical = 30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CustomText(
+                text = text,
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.secondary,
+                        shape = RoundedCornerShape(50.dp)
+                    )
+                    .padding(vertical = 8.dp, horizontal = 16.dp),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 
 }
