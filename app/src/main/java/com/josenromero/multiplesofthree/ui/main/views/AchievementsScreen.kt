468b93921da1d7e6c719c51805e4f7286b9d71fd
Jose <romerojosemanuel@hotmail.com>
2025-02-16 17:00:07 -0600

    update AchievementsScreen

--- app/src/main/java/com/josenromero/multiplesofthree/ui/main/views/AchievementsScreen.kt
+++ app/src/main/java/com/josenromero/multiplesofthree/ui/main/views/AchievementsScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
@@ -44,12 +43,10 @@ fun AchievementsScreen(
                 .padding(it)
         ) {
             LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
+                columns = GridCells.Fixed(1),
                 contentPadding = PaddingValues(16.dp)
             ) {
-                item(
-                    span = { GridItemSpan(currentLineSpan = 2) }
-                ) {
+                item {
                     AchievementStatistics(
                         totalAchievements = Constants.achievementsList.size,
                         currentAchievements = achievements.size
